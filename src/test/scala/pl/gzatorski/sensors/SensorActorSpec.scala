60cf1ededae1dc7e5d323626e32a973e6d64346b
Grzegorz Zatorski <grzegorz.zatorski@neurosoft.pl>
2019-05-23 11:33:33 +0200

    Test Sensor Actor

---
+++ src/test/scala/pl/gzatorski/sensors/SensorActorSpec.scala
@@ -0,0 +1,61 @@
+package pl.gzatorski.sensors
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+class SensorActorSpec extends TestKit(ActorSystem("SensorSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with Matchers
+  with BeforeAndAfterAll {
+  
+  override def afterAll: Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "An Sensor actor" must {
+
+    "handle single result" in {
+      val actor = TestActorRef[SensorActor]
+      actor ! ProcessSingleMeasurement(Measurement("s1", Some(50)))
+      actor ! FinishProcessing()
+
+      expectMsg(PartialComputation(ComputationResult("s1", Some(50), Some(50), 50, 1, 0)))
+    }
+
+    "handle NaN as Nones" in {
+      val actor = TestActorRef[SensorActor]
+      actor ! ProcessSingleMeasurement(Measurement("s1", None))
+      actor ! FinishProcessing()
+
+      expectMsg(PartialComputation(ComputationResult("s1", None, None, 0, 0, 1)))
+    }
+
+    "sum values" in {
+      val actor = TestActorRef[SensorActor]
+      actor ! ProcessSingleMeasurement(Measurement("s1", Some(5)))
+      actor ! ProcessSingleMeasurement(Measurement("s1", Some(10)))
+      actor ! ProcessSingleMeasurement(Measurement("s1", Some(15)))
+
+      actor ! FinishProcessing()
+
+      expectMsg(PartialComputation(ComputationResult("s1", Some(5), Some(15), 30, 3, 0)))
+    }
+
+    "count failed and successful events" in {
+      val actor = TestActorRef[SensorActor]
+      actor ! ProcessSingleMeasurement(Measurement("s1", Some(0)))
+      actor ! ProcessSingleMeasurement(Measurement("s1", None))
+      actor ! ProcessSingleMeasurement(Measurement("s1", Some(100)))
+      actor ! ProcessSingleMeasurement(Measurement("s1", None))
+      actor ! ProcessSingleMeasurement(Measurement("s1", Some(9)))
+      actor ! ProcessSingleMeasurement(Measurement("s1", None))
+      
+      actor ! FinishProcessing()
+
+      expectMsg(PartialComputation(ComputationResult("s1", Some(0), Some(100), 109, 3, 3)))
+    }
+    
+  }
+}
